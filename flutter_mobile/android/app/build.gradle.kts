c44055098c8a7b0b6a64188884e0e47ce6a6f653
strivo23 <chandrakanthreddyy687@gmail.com>
2026-07-23 16:40:30 +0530

    Configure Firebase, update Android build, and improve backend connectivity

--- flutter_mobile/android/app/build.gradle.kts
+++ flutter_mobile/android/app/build.gradle.kts
@@ -11,9 +11,10 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+    isCoreLibraryDesugaringEnabled = true
+}
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
@@ -43,4 +44,7 @@ kotlin {
 
 flutter {
     source = "../.."
+}
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.5")
 }
